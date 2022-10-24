bb1a3a70ab661cbdd34b78e54d5765024c5c96a5
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-10-24 20:11:16 +0200

    Add project files.

---
+++ SQLite_Core_EF/DemoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLite_Core_EF
+{
+    public class DemoContext : DbContext
+    {
+        public DbSet<UserModel> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite(@"Data Source=C:\Temp\demo.db");
+    }
+}
